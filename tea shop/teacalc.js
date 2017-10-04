e57938fbd45edd97db51e5762201095f04c44dca
ccpeterson <clinton.c.peterson@gmail.com>
2017-10-03 19:55:23 -0700

    class work

--- tea shop/teacalc.js
+++
@@ -1,3 +0,0 @@
-var name;
-name = prompt("What is your name?")
-document.getElementById("username").innerHTML = name;
---
+++ week2/variables.js
@@ -0,0 +1,46 @@
+/*
+Clinton Peterson
+2 Oct 2017
+practice wqith variables
+*/
+
+//declaring the variable
+var value;
+
+//assign the variable, then write it
+value = "Hello World!";
+
+document.write(value);
+
+//declaring and assigning
+var number = 100;
+
+//declaring multiple variables
+
+var street, city, state, zip;
+street = "100 1st st";
+city = "Seattle";
+state = "WA";
+zip = 99999;
+
+//javascript is a loosely typed lanuguage
+
+var favorite = "this is my fave!";
+console.log(favorite);
+
+favorite = 100;
+
+console.log(favorite);
+
+favorite = true;
+
+console.log(favorite);
+
+//mix?
+var mix = [1, true, "woo"];
+console.log(mix);
+
+
+var place = "seattle seattle tacoma wa";
+
+console.log(place.replace("seattle","poop"));
